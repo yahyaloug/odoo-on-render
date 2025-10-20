e055e7cd6ca5c7a99477a08b2294daf47b353579
yahyaloug <96186497+yahyaloug@users.noreply.github.com>
2025-10-20 19:33:39 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,6 +32,13 @@ http_port = ${PORT}
 db_filter = ^${DB_NAME}$
 list_db = False
 proxy_mode = True
+workers = 2
+max_cron_threads = 1
+limit_memory_hard = 2684354560
+limit_memory_soft = 2147483648
+limit_request = 8192
+limit_time_cpu = 600
+limit_time_real = 1200
 EOC
 
 mkdir -p /var/lib/odoo
@@ -41,4 +48,4 @@ cat /etc/odoo/odoo.conf
 echo "=========================="
 
 echo "=== Starting Odoo ==="
-exec odoo -c /etc/odoo/odoo.conf
+exec odoo -c /etc/odoo/odoo.conf --load-language=en_US
